93faae6d46bff1a7ab923f0a592a5a87771b73ad
tiagosoussa7 <tiago.soussa@live.com>
2024-03-15 14:21:20 -0300

    envio de intermediário de validação do corpo body.

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { register_user } = require('../controllers/user');
+const { body_validation } = require('../middleware/validation');
+const { schema_register } = require('../schemas/users_schema');
 
 const route_user = express();
 
-route_user.post('/usuario', 
+route_user.post('/usuario', body_validation( schema_register ),
     register_user
 );
 
--- src/schemas/users_schema.js
+++ src/schemas/users_schema.js
@@ -1,12 +1,17 @@
 const joi = require('joi');
 
 const schema_register = joi.object({
-    instituicao_nome: joi.string().required().messages({
+    nome: joi.string().required().messages({
         'any.required': 'Cadastro negado: o campo  nome é obrigatório.',
         'string.empty': 'Cadastro negado: o campo  nome está vazio.',
         'string.base': 'Cadastro negado: preencha corretamente o campo nome.',
     }),
-    instituicao_senha: joi.string().min(4).required().messages({
+    email: joi.string().email().required().messages({
+        'string.email': 'Cadastro negado: o email não é válido.',
+        'string.base': 'Cadastro negado: o email não é válido.',
+        'any.required': 'Cadastro negado: o campo email é obrigatório.'
+    }),
+    senha: joi.string().min(4).required().messages({
         'any.required': 'Cadastro negado: o campo senha é obrigatório.',
         'string.base': 'Cadastro negado: preencha corretamente o campo senha.',
         'string.min': 'Cadastro negado: a senha precisa ter no mínimo 4 caracteres.',
