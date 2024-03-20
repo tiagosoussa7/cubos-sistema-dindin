909398fb87077e554c2dbaa966c51917bccdbd81
tiagosoussa7 <tiago.soussa@live.com>
2024-03-20 10:40:02 -0300

    configuração cors.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors'); 
 
 const route_login = require('./routes/login_route');
 const route_user = require('./routes/user_routes');
@@ -8,6 +9,7 @@ const route_transaction = require('./routes/transaction_routes');
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(route_login);
